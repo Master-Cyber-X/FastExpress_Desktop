c7fd12ee54ab542474c624abb0151fca728120af
Sami <user0089kpg@gmail.com>
2025-10-08 11:18:51 +0300

    Update Test File

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from ttkbootstrap import Window, Frame, Label
 from config.Libaries import *
 
-root = Window(themename='cyborg')
+root = Window(themename='flatly')
 root.title('Details')
 root.geometry('1450x850+200+50')
 
@@ -13,87 +13,200 @@ py = 10
 px = 10
 pxv = 30
 
+head_ft = font=('Times',17,'bold') # Head Title Font
+val_ft = font=('Times',13,'bold')  # Value Title Font
 
-def shipment_details():
-    'تفاصيل الشحنة'
-    f0 = LabelFrame(scrf, text='تفاصيل الشحنة', bootstyle='danger')
-    f0.pack(fill='both', padx=10, pady=4)
-
-    f00 = Frame(f0, )
-    f00.pack(fill='both', side='right')
-
-    l00 = Label(f00, text=':رقم الشحنة', font=('Times',15,'bold'))
-    l00.pack(pady=10)
-
-    l01 = Label(f00, text='JED-RYD-90304697', font=('Times',13,'bold'))
-    l01.pack(pady=4)
-
-    l02 = Label(f00, text='عدد الطرود', font=('Times',15,'bold'))
-    l02.pack(pady=10)
-
-    l03 = Label(f00, text='4', font=('Times',13,'bold'))
-    l03.pack(pady=10)
-
-    l04 = Label(f00, text='السعر', font=('Times',15,'bold'))
-    l04.pack(pady=10)
-
-    l05 = Label(f00, text='230.00', font=('Times',13,'bold'))
-    l05.pack(pady=4, padx=10)
-
-    l06 = Label(f00, text='الوجهه', font=('Times',15,'bold'))
-    l06.pack(pady=10)
-
-    l07 = Label(f00, text='RIDHUB', font=('Times',13,'bold'))
-    l07.pack(pady=4, padx=10)
-
-# shipment_details()
 
 
 
 
 def sender_details():
     'تفاصيل المرسل'
-    
-    b = LabelFrame(scrf, text='بيانات المرسل', bootstyle='danger')
+    b = LabelFrame(scrf, text='بيانات المرسل', bootstyle='PRIMARY')
     b.pack(fill='both', padx=px, pady=py)
     
+
+    def Right_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='right')
+        b0.grid(row=0, column=2)
+        
+        f00 = Frame(b0)  # SubFrame> # SubFrame
+        f00.pack(fill='both')
+        l00 = Label(f00, text='أسم المرسل', font=head_ft)
+        l00.pack( padx=px, pady=py, side='right')
+
+        f01 = Frame(b0)  # SubFrame> # SubFrame
+        f01.pack(fill='both')
+        l01 = Label(f01, text='محمد حسن عبدالعزيز الفالحي', font=val_ft)
+        l01.pack( padx=pxv, pady=py, side='right')
+
+
+        f02 = Frame(b0)  # SubFrame> # SubFrame
+        f02.pack(fill='both')
+        l02 = Label(f02, text='رقم المرسل', font=head_ft)
+        l02.pack(padx=px, pady=py, side='right')
+
+        f03 = Frame(b0)
+        f03.pack(fill='both')
+        l03 = Label(f03, text='0587675676', font=val_ft)
+        l03.pack(padx=pxv, pady=py, side='right')
     
-    f00 = Frame(b)  # SubFrame> # SubFrame
-    f00.pack(fill='both')
-    l00 = Label(f00, text='أسم المرسل', font=('Times',15,'bold'))
-    l00.pack( padx=px, pady=py, side='right')
-
-    f01 = Frame(b)  # SubFrame> # SubFrame
-    f01.pack(fill='both')
-    l01 = Label(f01, text='محمد حسن عبدالعزيز الفالحي', font=('Times',13,'bold'))
-    l01.pack( padx=pxv, pady=py, side='right')
 
 
+    def Center_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py)
+        b0.grid(row=0, column=1)
+        
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='المدينة', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='الرياض', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='العنوان', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='الرياض-حي الزهور', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+
+    def Left_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='left')
+        b0.grid(row=0, column=0)
+
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='خطوط الطول', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='24.54343774677', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='خطوط العرض', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='24.134979718', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+
+    Right_Frame()
+    Left_Frame()
+    Center_Frame()
+
+    b.columnconfigure(1, weight=1)
 
 
-    f02 = Frame(b)  # SubFrame> # SubFrame
-    f02.pack(fill='both')
-    l02 = Label(f02, text=':رقم المرسل', font=('Times',15,'bold'))
-    l02.pack(padx=px, pady=py, side='right')
 
-    f03 = Frame(b)
-    f03.pack(fill='both')
-    l03 = Label(f03, text='0587675676', font=('Times',13,'bold'))
-    l03.pack(padx=pxv, pady=py, side='right')
+def receiver_details():
+    'تفاصيل المستلم'
+    b = LabelFrame(scrf, text='بيانات المستلم', bootstyle='PRIMARY')
+    b.pack(fill='both', padx=px, pady=py)
+    
+
+    def Right_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='right')
+        b0.grid(row=0, column=2)
+        
+        f00 = Frame(b0)  # SubFrame> # SubFrame
+        f00.pack(fill='both')
+        l00 = Label(f00, text='أسم المستلم', font=head_ft)
+        l00.pack( padx=px, pady=py, side='right')
+
+        f01 = Frame(b0)  # SubFrame> # SubFrame
+        f01.pack(fill='both')
+        l01 = Label(f01, text='خالد حمد المالكي', font=val_ft)
+        l01.pack( padx=pxv, pady=py, side='right')
 
 
-    f04 = Frame(b)
-    f04.pack(fill='both')
-    l04 = Label(f00, text='المدينة', font=('Times',15,'bold'))
-    l04.pack(padx=px, pady=py, side='right')
+        f02 = Frame(b0)  # SubFrame> # SubFrame
+        f02.pack(fill='both')
+        l02 = Label(f02, text='رقم المستلم', font=head_ft)
+        l02.pack(padx=px, pady=py, side='right')
 
-    f05 = Frame(b)
-    f05.pack(fill='both')
-    l05 = Label(f00, text='RIDHUB', font=('Times',13,'bold'))
-    l05.pack(padx=pxv, pady=py, side='right')
+        f03 = Frame(b0)
+        f03.pack(fill='both')
+        l03 = Label(f03, text='0597967914', font=val_ft)
+        l03.pack(padx=pxv, pady=py, side='right')
+    
 
-# shipment_details()
+
+    def Center_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py)
+        b0.grid(row=0, column=1)
+        
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='المدينة', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='الرياض', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='العنوان', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='جدة-حي الزهور', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+
+    def Left_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='left')
+        b0.grid(row=0, column=0)
+
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='خطوط الطول', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='24.54343774677', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='خطوط العرض', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='24.134979718', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+
+    Right_Frame()
+    Left_Frame()
+    Center_Frame()
+
+    b.columnconfigure(1, weight=1)
+
 
 sender_details()
+receiver_details()
+
+
+
 
 root.mainloop()
