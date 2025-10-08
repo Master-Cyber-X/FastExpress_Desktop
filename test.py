0cdbb1fb034d8a520a81dd62ed78ef9a060e4c8e
Sami Express PC <user0089kpg@gmail.com>
2025-10-08 14:40:56 +0300

    Update test File and add img riyal soudi arabic as png

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
 from ttkbootstrap import Window, Frame, Label
 from config.Libaries import *
 
-root = Window(themename='flatly')
+
+
+root = Window(themename='CustomTheme')
 root.title('Details')
 root.geometry('1450x850+200+50')
+
+sar_img = ImageTk.PhotoImage(Image.open(r'C:\Users\Exp\Downloads\Saudi_Riyal_Symbol.png').resize((15,15)))
+
+
 
 scrf = ScrolledFrame(root, bootstyle='secondray')
 scrf.pack(fill='both', expand=True)
@@ -13,7 +19,7 @@ py = 10
 px = 10
 pxv = 30
 
-head_ft = font=('Times',17,'bold') # Head Title Font
+head_ft = font=('Times',20,'bold') # Head Title Font
 val_ft = font=('Times',13,'bold')  # Value Title Font
 
 
@@ -111,7 +117,6 @@ def sender_details():
     b.columnconfigure(1, weight=1)
 
 
-
 def receiver_details():
     'تفاصيل المستلم'
     b = LabelFrame(scrf, text='بيانات المستلم', bootstyle='PRIMARY')
@@ -158,7 +163,7 @@ def receiver_details():
 
         f05 = Frame(b0)
         f05.pack(fill='both')
-        l05 = Label(f05, text='الرياض', font=val_ft)
+        l05 = Label(f05, text='الدمام', font=val_ft)
         l05.pack(padx=pxv, pady=py, side='right')
         
         f06 = Frame(b0)
@@ -168,7 +173,7 @@ def receiver_details():
 
         f07 = Frame(b0)
         f07.pack(fill='both')
-        l05 = Label(f07, text='جدة-حي الزهور', font=val_ft)
+        l05 = Label(f07, text='جدة-حي السعد', font=val_ft)
         l05.pack(padx=pxv, pady=py, side='right')
 
     def Left_Frame():
@@ -203,10 +208,159 @@ def receiver_details():
     b.columnconfigure(1, weight=1)
 
 
+def shipment_details():
+    'تفاصيل الشحنة'
+    b = LabelFrame(scrf, text='بيانات الشحنة', bootstyle='PRIMARY')
+    b.pack(fill='both', padx=px, pady=py)
+    
+
+    def Right_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='right')
+        b0.grid(row=0, column=3)
+        
+        f00 = Frame(b0)  # SubFrame> # SubFrame
+        f00.pack(fill='both')
+        l00 = Label(f00, text='نوع الشحنة', font=head_ft)
+        l00.pack( padx=px, pady=py, side='right')
+
+        f01 = Frame(b0)  # SubFrame> # SubFrame
+        f01.pack(fill='both')
+        l01 = Label(f01, text='كرتون', font=val_ft)
+        l01.pack( padx=pxv, pady=py, side='right')
+
+
+        f02 = Frame(b0)  # SubFrame> # SubFrame
+        f02.pack(fill='both')
+        l02 = Label(f02, text='العدد', font=head_ft)
+        l02.pack(padx=px, pady=py, side='right')
+
+        f03 = Frame(b0)
+        f03.pack(fill='both')
+        l03 = Label(f03, text='14', font=val_ft)
+        l03.pack(padx=pxv, pady=py, side='right')
+    
+
+
+    def Center_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py)
+        b0.grid(row=0, column=2)
+        
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='السعر', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='250', font=val_ft, image=sar_img, compound='left')
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='الضريبة', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='37.5', font=val_ft, image=sar_img, compound='left')
+        l05.pack(padx=pxv, pady=py, side='right')
+
+    def Left_Frame():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='left')
+        b0.grid(row=0, column=1)
+
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='الإجمالي شامل الضريبة', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='287.5', font=val_ft, image=sar_img, compound='left')
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='سعر التوصيل شامل الضريبة', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='70', font=val_ft, image=sar_img, compound='left')
+        l05.pack(padx=pxv, pady=py, side='right')
+
+    def Left_Frame1():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='left')
+        b0.grid(row=0, column=0)
+
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='الإجمالي الكلي', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='357.5', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='ملاحضة', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='قابل للكسر', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+
+
+    def Left_Frame2():
+        b0 = Frame(b, bootstyle='danger')
+        # b0.pack(fill='both', padx=px, pady=py, side='left')
+        b0.grid(row=0, column=0)
+
+        f04 = Frame(b0)
+        f04.pack(fill='both')
+        l04 = Label(f04, text='نوع الألتقاط', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f05 = Frame(b0)
+        f05.pack(fill='both')
+        l05 = Label(f05, text='إستلام من المستودع', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+        
+        f06 = Frame(b0)
+        f06.pack(fill='both')
+        l04 = Label(f06, text='المستخدم/المندوب', font=head_ft)
+        l04.pack(padx=px, pady=py, side='right')
+
+        f07 = Frame(b0)
+        f07.pack(fill='both')
+        l05 = Label(f07, text='R-5009', font=val_ft)
+        l05.pack(padx=pxv, pady=py, side='right')
+
+
+
+    Right_Frame()
+    Left_Frame()
+    Left_Frame1()
+    Left_Frame2()
+    Center_Frame()
+
+    b.columnconfigure(1, weight=2)
+    b.columnconfigure(2, weight=2)
+
+
+
+
+
 sender_details()
 receiver_details()
-
-
+shipment_details()
 
 
 root.mainloop()
